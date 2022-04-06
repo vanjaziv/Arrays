c807b1ac5bf659cc5bc864c760f25e4814719a15
vanjaziv <vanja.zivanic1@gmail.com>
2022-04-06 22:34:41 +0200

    Podsjetnik

---
+++ src/ObjectKlasse/Podsjetnik.java
@@ -0,0 +1,127 @@
+package ObjectKlasse;
+
+// public class Podsjetnik {
+    /*public class MethodExample {
+
+        public void add() {
+            System.out.println("Addition method");
+        }
+
+        public static void main(String[] args) {
+            MethodExample m = new MethodExample();
+            m.add();
+        }
+    }
+—--------------------------------
+    Metoda bez parametra i bez povratnog tipa
+
+    public class Addition {
+        int x,y;
+        Addition(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void add() {
+            int sum = x + y;
+            System.out.println("Sum is: " + sum);
+        }
+        public static void main(String[] args) {
+            Addition a = new Addition(5,8);
+            a.add();
+        }
+    }
+—--------------------------------
+    Metoda s parametrima, ali nema tip povratka
+
+    public class Subtraction {
+
+        public void difference(int x, int y) {
+            int diff = x - y;
+            System.out.println("Difference is: " + diff);
+        }
+        public static void main(String[] args) {
+            Subtraction s = new Subtraction();
+            s.difference(10, 4);
+        }
+    }
+—--------------------------------
+
+
+
+    Metoda koja ima tip povratka, ali nema parametre
+
+    public class Multiplication {
+        int product,x, y;
+
+        Multiplication(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int multiply() {
+            product = x * y;
+            return product;
+        }
+        public static void main(String[] args) {
+            Multiplication m = new Multiplication(4,8);
+            int prod = m.multiply();
+            System.out.println("Product is: " + prod);
+        }
+    }
+—------------------------------------------------
+    Metoda s parametrom i tipom povratka
+
+    public class Compare {
+        int max;
+
+        public int greaterNumber(int x, int y) {
+            if(x > y)
+                max = x;
+            else
+                max = y;
+
+            return max;
+        }
+        public static void main(String[] args) {
+            Compare c = new Compare();
+            int value = c.greaterNumber(47, 29);
+            System.out.println("Greater value is: " + value);
+        }
+    }
+—---------------------------------------------
+
+
+
+
+
+
+
+    Instance metode
+
+    public class MethodExample {
+
+        //Static method
+        public static void add() {
+            System.out.println("Addition method");
+        }
+
+        //Instance method
+        public void subtract() {
+            System.out.println("Subtraction method");
+        }
+        public static void main(String[] args) {
+            MethodExample m = new MethodExample();
+
+            //Invoking a static method
+            add();
+
+            //Invoking an instance method
+            m.subtract();
+        }
+    }
+
+—------------------------------------------------
+
+
+}*/
