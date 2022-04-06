a122b3079a1d11964cbb249450b4c1c9447aceea
vanjaziv <99715839+vanjaziv@users.noreply.github.com>
2022-04-06 19:22:36 +0200

    Update Rectangle.java

--- src/ObjectKlasse/Rectangle.java
+++ src/ObjectKlasse/Rectangle.java
@@ -1,5 +1,47 @@
 package ObjectKlasse;
 
+package at.campus02.iwi.pictures;
+
+public class Picture {
+    int lenght;
+    int width;
+
+    public int totalPixels(){
+        return lenght*width;
+    }
+
+    public int totalSize(int bytePerPixel){
+        return bytePerPixel * totalPixels();
+    }
+
+    public void scale(double factor){
+        lenght = (int) (lenght * factor);
+        width *= factor;
+    }
+
+}
+
+App--------------------
+     
+public class PictureApp {
+    public static void main(String[] args) {
+      Picture p = new Picture();
+      p.width = 200;
+      p.lenght = 100;
+
+        System.out.println(p.totalPixels());
+
+        int totalSize = p.totalSize(2);
+        System.out.println(totalSize);
+
+        p.scale(0.5);
+        System.out.println(p.width + " " + p.lenght
+        );
+    }
+}
+
+
+
 public class Rectangle {
         int length;
         int width;
